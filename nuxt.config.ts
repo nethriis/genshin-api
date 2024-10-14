80970468343c95e022d1554939729619b967a458
nethris <david.abou@epitech.eu>
2024-10-14 03:44:35 -0400

    fix: change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Teyvat Dev'
+      title: 'Home'
     }
   },
   css: ['~/assets/css/main.css'],
