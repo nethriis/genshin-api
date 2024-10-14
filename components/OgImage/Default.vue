37fe91cd6e800eea93ec94f5b99db02df73f3ba2
nethris <david.abou@epitech.eu>
2024-10-14 03:58:39 -0400

    fix: set og image background color to white

--- components/OgImage/Default.vue
+++ components/OgImage/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-screen flex items-center justify-center">
+  <div class="w-full h-screen flex items-center justify-center bg-white">
     <div class="flex flex-col items-center">
       <h1 class="text-6xl text-center text-neutral-900 font-bold">
         The Teyvat Developers
