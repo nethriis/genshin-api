d84766943ca0c0fe1b5bc33141667585a9c71300
nethris <david.abou@epitech.eu>
2024-10-14 18:13:43 -0400

    feat: add artifacts route to root api

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async () => ({
   characters: 'https://teyvat-dev.vercel.app/api/characters',
   weapons: 'https://teyvat-dev.vercel.app/api/weapons',
+  artifacts: 'https://teyvat-dev.vercel.app/api/artifacts',
   materials: 'https://teyvat-dev.vercel.app/api/materials',
   nations: 'https://teyvat-dev.vercel.app/api/nations',
   elements: 'https://teyvat-dev.vercel.app/api/elements'
